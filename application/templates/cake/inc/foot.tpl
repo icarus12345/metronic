de7977a48ef4f71169fc5912c13bfb1af900b24f
icarus12345 <xuantruong@jv-it.com.vn>
2016-04-08 16:53:54 +0700

    DCLBAN

--- application/templates/cake/inc/foot.tpl
+++ application/templates/cake/inc/foot.tpl
@@ -1,23 +1,95 @@
-[{strip}]
-<div class="space-line"></div>
-<div class="space-line"></div>
 <div class="footer">
-    <div class="foot-rem"></div>
-    <div>
-        <div class="container">
-            <div class="socials">
-
-                <a href="[{$settting_data['facebook']|default:''|escape:'html'}]"><img src="/assets/cake/images/f.png"/></a>
-                <a href="[{$settting_data['google']|default:''|escape:'html'}]"><img src="/assets/cake/images/g.png"/></a>
-                <a href="[{$settting_data['twitter']|default:''|escape:'html'}]"><img src="/assets/cake/images/t.png"/></a>
-            </div>
-            <div class="foot-hr"></div>
-            <div class="foot-info">
-                <span><i class="fa fa-home"></i> [{$settting_data['address']|default:''|escape:'html'}]</span>
-                <span><i class="fa fa-envelope-o"></i> [{$settting_data['email']|default:''|escape:'html'}]</span>
-                <span><i class="fa fa-phone"></i> [{$settting_data['phone']|default:''|escape:'html'}]</span>
+            <div class="container">
+                <div class="row">
+                    <div class="space-line"></div>
+                    <div class="space-line"></div>
+                        
+                        <div class="col-sm-4 list-info">
+                            <div class="titl"><b>THÔNG TIN</b></div>
+                            <div class="space-line"></div>
+                            <ul>
+                                <li><a href="#">CỬA HÀNG DỤNG CỤ LÀM BÁNH AN PHÚ</a></li>
+                                <li><a href="#"><i class="fa fa-map-marker"></i> 108/46 Trần Quang Diệu P.10 Q.3 TP.HCM</a>
+                                <li><a href="#"><i class="fa fa-mobile"></i> 098 263 6618</a></li>
+                                <li><a href="#"><i class="fa fa-envelope"></i> hotro@dungculambanhanphu.com</a></li>
+                            </ul>
+                            <div class="space-line"></div>
+                            <div class="space-line"></div>
+                        </div>
+                        <div class="col-sm-4 list-info">
+                            <div class="titl"><b>SẢN PHẨM MỚI</b></div>
+                            <div class="space-line"></div>
+                            <div class="cake-item-small2">
+                                <div class="line-clamp-1">Súng bơm cookies (kim loại/nhựa)</div>
+                                <div class="space-line-half"></div>
+                                <div class="">
+                                    <i class="fa fa-tags"></i> Thiết bị
+                                </div>
+                                <div class="product-price line-clamp-1">120000-150000</div>
+                                <div class="nailthumb">
+                                    <div class="nailthumb-figure-square">
+                                        <a href="#" class="nailthumb-container">
+                                            <img data-original="assets/imgs/1.jpg" class="lazy"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="cake-item-small2">
+                                <div class="line-clamp-1">Súng bơm cookies (kim loại/nhựa)</div>
+                                <div class="space-line-half"></div>
+                                <div class="">
+                                    <i class="fa fa-tags"></i> Thiết bị
+                                </div>
+                                <div class="product-price line-clamp-1">120000-150000</div>
+                                <div class="nailthumb">
+                                    <div class="nailthumb-figure-square">
+                                        <a href="#" class="nailthumb-container">
+                                            <img data-original="assets/imgs/1.jpg" class="lazy"/>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="space-line"></div>
+                            <div class="space-line"></div>
+                        </div>
+                        <div class="col-sm-4 list-info ">
+                            <div class="titl"><b>TÌM KIẾM NHIỀU NHẤT</b></div>
+                            <div class="space-line"></div>
+                            <div class="tags">
+                            <a href="#">dụng cụ làm bánh</a>
+                            <a href="#">lò lướng bánh</a>
+                            <a href="#">bột</a>
+                            <a href="#">thiết bị</a>
+                            <!-- <a href="#">nguyên liệu làm bánh</a> -->
+                            </div>
+                            <div class="titl"><b>WEBSITE LIÊN KẾT</b></div>
+                            <div class="space-line"></div>
+                            <a href="#">banhyeu.com</a><br/>
+                            <a href="#">banhngononline.com</a><br/>
+                            <a href="#">nulljean.com</a>
+                            <div class="space-line"></div>
+                            <div class="space-line"></div>
+                        </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-[{/strip}]
+        <div class="footer-menu">
+            <div class="container">
+                <a href="#">Trang chủ</a>
+                <a href="#">Trang chủ</a>
+                <a href="#">Trang chủ</a>
+                <a href="#">Trang chủ</a>
+                <a href="#">Trang chủ</a>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="space-line"></div>
+                    <div class="space-line"></div>
+                    <div class="col-sm-6">Power by Creative Design Studio</div>
+                    <div class="col-sm-6 text-right">Power by Creative Design Studio</div>
+                    <div class="clearfix"></div>
+                    <div class="space-line"></div>
+                    <div class="space-line"></div>
+                </div>
---
+++ assets/cake/js/comic.js
@@ -0,0 +1,136 @@
+$(document).ready(function(){
+    $("img.lazy").lazyload({
+       load: function() {
+           $(this).removeClass('lazy');
+           $(this.parentNode).nailthumb();
+       }
+    });
+    // $("img.lazy").each(function(){
+    //     $(this).attr('src',$(this).data('original'));
+    //     $(this.parentNode).nailthumb();
+    // });
+    if($('#owl-upcoming').length==1){
+        $("#owl-upcoming").owlCarousel({
+            autoPlay: false,
+            items : 1,
+            lazyLoad: true,
+            nav : true,
+            navText : ["", ""],
+            responsive : {
+                0:      {items: 1, nav: true},
+                480:    {items: 2, nav: true},
+                960:    {items: 3, nav: true},
+                1200:   {items: 1, nav: true},
+            }
+        });
+    }
+    if($('#owl-rank').length==1){
+        $("#owl-rank").owlCarousel({
+            autoPlay: false,
+            items : 3,
+            lazyLoad: true,
+            nav : true,
+            navText : ['<i class="fa fa-angle-left fa-2"></i>', '<i class="fa fa-angle-right fa-2"></i>'],
+            responsive : {
+                0:      {items: 1, nav: true},
+                480:    {items: 2, nav: true},
+                960:    {items: 3, nav: true},
+                1200:   {items: 3, nav: true},
+            }
+        });
+    }
+    if($('#owl-related').length==1){
+        $("#owl-related").owlCarousel({
+            autoPlay: false,
+            items : 3,
+            lazyLoad: true,
+            nav : true,
+            navText : ['<i class="fa fa-angle-left fa-2"></i>', '<i class="fa fa-angle-right fa-2"></i>'],
+            responsive : {
+                0:      {items: 1, nav: true},
+                480:    {items: 2, nav: true},
+                960:    {items: 3, nav: true},
+                1200:   {items: 4, nav: true},
+            }
+        });
+    }
+    if($('#owl-cat-ucm').length==1){
+        $("#owl-cat-ucm").owlCarousel({
+            autoPlay: false,
+            items : 3,
+            lazyLoad: true,
+            nav : true,
+            navText : ['<i class="fa fa-angle-left fa-2"></i>', '<i class="fa fa-angle-right fa-2"></i>'],
+            responsive : {
+                0:      {items: 1, nav: true},
+                480:    {items: 2, nav: true},
+                768:    {items: 3, nav: true},
+                960:    {items: 4, nav: true},
+                1200:   {items: 4, nav: true},
+            }
+        });
+    }
+    if($('#owl-gallery').length==1){
+        $("#owl-gallery").owlCarousel({
+            autoPlay: false,
+            items : 2,
+            lazyLoad: true,
+            nav : true,
+            navText : ['', ''],
+            center: true,
+            loop:true,
+            mouseDrag:false,
+            touchDrag:false,
+            responsive : {
+                0:      {items: 1, nav: true},
+                768:    {items: 2, nav: true}
+            }
+        });
+    }
+    if($('.countdown').length>=1){
+    	$('.countdown').each(function(){
+    		$(this).html('<div class="countdown play"><ul class="time-count-down-flip time-count-down-days-ten days"><li style="line-height: 40px;" data-digit="3"><span><div class="up"><div class="shadow"></div><div class="inn">3</div></div><div class="down"><div class="shadow"></div><div class="inn">3</div></div></span></li></ul><ul class="time-count-down-flip time-count-down-days-one days"><li data-digit="1" style="line-height: 40px;" class="current"><span><div class="up"><div class="shadow"></div><div class="inn">1</div></div><div class="down"><div class="shadow"></div><div class="inn">1</div></div></span></li></ul><ul class="time-count-down-flip time-count-down-hours-ten hours"><li data-digit="2" style="line-height: 40px;" class="current"><span><div class="up"><div class="shadow"></div><div class="inn">2</div></div><div class="down"><div class="shadow"></div><div class="inn">2</div></div></span></li></ul><ul class="time-count-down-flip time-count-down-hours-one hours"><li data-digit="3" style="line-height: 40px;" class="current"><span><div class="up"><div class="shadow"></div><div class="inn">3</div></div><div class="down"><div class="shadow"></div><div class="inn">3</div></div></span></li></ul><ul class="time-count-down-flip time-count-down-minutes-ten minutes"><li data-digit="5" style="line-height: 40px;" class="current"><span><div class="up"><div class="shadow"></div><div class="inn">5</div></div><div class="down"><div class="shadow"></div><div class="inn">5</div></div></span></li></ul><ul class="time-count-down-flip time-count-down-minutes-one minutes"><li data-digit="4" style="line-height: 40px;" class="current"><span><div class="up"><div class="shadow"></div><div class="inn">4</div></div><div class="down"><div class="shadow"></div><div class="inn">4</div></div></span></li></ul><ul class="time-count-down-flip time-count-down-seconds-ten seconds"><li data-digit="4" style="line-height: 40px;" class="current"><span><div class="up"><div class="shadow"></div><div class="inn">4</div></div><div class="down"><div class="shadow"></div><div class="inn">4</div></div></span></li></ul><ul class="time-count-down-flip time-count-down-seconds-one seconds"><li data-digit="9" style="line-height: 40px;" class="current"><span><div class="up"><div class="shadow"></div><div class="inn">9</div></div><div class="down"><div class="shadow"></div><div class="inn">9</div></div></span></li></ul></div>');
+    	});
+	}
+    if($('.swipebox').length>=1)
+        $('a.swipebox').swipebox({useCSS: true});
+});
+$(window).resize(function(){
+	$('.nailthumb-image').each(function(){
+		$(this.parentNode).nailthumb();
+	});
+})
+function openPopup(){
+    $('#gallery-popup').dialog({
+        'modal': true,
+        /*autoOpen        : option.autoOpen,*/
+        'minwidth': '320px',
+        'dialogClass': 'gallery-popup',
+        'resizable': false,
+        'width': 'auto',
+        'title': null,
+        'closeOnEscape': true,
+        // 'position': 'top',
+        /*hide                : "explode",*/
+        'buttons': null,
+        'open': function(event, ui) {
+            $(event.target).dialog('widget')
+                .css({
+                    // 'position': 'fixed'
+                })
+                .position({
+                    'my': 'center',
+                    'at': 'center',
+                    'of': window
+                });
+        },
+        'close': function(event, ui) {
+            
+        },
+        'create': function() {
+            $(this).closest(".ui-dialog")
+                .find(".ui-dialog-titlebar")
+                .hide();
+        }
+    });
+} 
